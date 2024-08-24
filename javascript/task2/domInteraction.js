a8f2557e169d4001a51f9c35088ebb87db023150
Florian Daumas <fdaumas@Florians-MacBook-Pro.local>
2024-08-24 23:30:36 +0200

    mv all js exercise in directory + correct task2 Hello, check -> Hello, Check!

--- javascript/task2/domInteraction.js
+++ javascript/task2/domInteraction.js
@@ -2,7 +2,7 @@ function changeText() {
   const elem = document.getElementById("interaction-text");
   
   if (elem.textContent == "myText")
-    elem.innerText = "Hello, check";
+    elem.innerText = "Hello, Check!";
   else 
     console.log("you already check this button");
 }
